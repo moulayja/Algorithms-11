70513745552e0169436d0141cc88b502c50eb8c9
Jamal <moulayjam@gmail.com>
2020-07-04 12:18:37 -0400

    Power Ranger(Solution)

--- Power Ranger.js
+++ Power Ranger.js
@@ -4,3 +4,13 @@ Create a function that takes in n, a, b and returns the number of values raised
 to the nth power that lie in
 the range [a, b], inclusive.
 */
+
+function powerRanger(power, min, max) {
+	var c = 0;
+	for (var i = 1; ; i++) {
+		var n = Math.pow(i, power);
+		if (n > max) break;
+		if (n >= min) c++;
+	}
+	return c;
+}
